b6606b100401e072efe6472f2983f0caac9f81f2
ren <tyatyareat14@gmail.com>
2025-03-08 14:25:12 +0900

    マイグレーション

---
+++ src/app/Models/Product_Season.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product_Season extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'product_id',
+        'season_id',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function season()
+    {
+        return $this->belongsTo(Season::class);
+    }
+}
---
+++ src/database/seeders/SeasonsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SeasonsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'name' => '春',
+                        
+        ];
+        $param = [
+            'name' => '夏',
+
+        ];
+        $param = [
+            'name' => '秋',
+
+        ];
+        $param = [
+            'name' => '冬',
+
+        ];
+    }
+}
